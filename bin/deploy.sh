634ff1005908af153428548d4b5ff07a0b5ac8c2
Andrey <andrew.semikov@gmail.com>
2015-05-04 17:28:57 +0300

    Add comments to deploy script. More verbosity

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 # terminate uwsgi emperror process
+echo "Terminating uwsgi emperor..."
 kill -15 `ps -ef | grep 'uwsgi --emperor' | grep -v grep | awk '{print $2}'`
 
+echo "Launching uwsgi emperor..."
 uwsgi --emperor /etc/uwsgi/vassals --uid www-data --gid www-data --daemonize /var/projects/lifeline-latincheck/logs/uwsgi/uwsgi-emperor.log
 
 # generate assets
+echo "Generating assets..."
 cd /var/projects/lifeline-latincheck
 python manage.py collectstatic --noinput
