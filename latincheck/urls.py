3df9ad1d470987ed8dd5395242d60171c9cd9db5
Andrey <andrew.semikov@gmail.com>
2015-05-05 15:11:33 +0300

    Add robots.txt file

--- latincheck/urls.py
+++ latincheck/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 # from django.conf.urls import handler404
 # from django.contrib import admin
+from django.views.generic import TemplateView
 from mainapp.views import ProcessUrlFormView, IndexView, AboutView
 from django.conf.urls.i18n import i18n_patterns
 
@@ -8,8 +9,8 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'latincheck.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     url(r'^validate', ProcessUrlFormView.as_view(), name='process_url'),
     # url(r'^about$', AboutView.as_view(), name='about'),
     # url(r'^$', IndexView.as_view(), name='home'),
